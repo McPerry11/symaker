915ce3ced915b8ef819b9db8d410cedaf19e567d
jebjebs <virgiliolopez.jev@gmail.com>
2020-05-12 21:21:42 +0800

    V0.3.0 - Navbar

---
+++ resources/views/_navbar.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar box-shadow-y">
+	<div class="navbar-brand">
+		<a href="#" class="navbar-item is-size-3 has-text-weight-bold has-text-primary">SyMaker 2.0</a>
+		<a href="#" class="navbar-burger nav-toggler">
+			<span></span>
+			<span></span>
+			<span></span>
+		</a>
+	</div>
+	<div class="navbar-menu">
+		<div class="navbar-start">
+			<div class="navbar-item breadcrumb has-succeeds-separator" aria-label="breadcrumbs">
+				<ul>
+					<li><a href="#"></a></li>
+					<li class="is-active">
+						<a href="#" aria-current="page" class="is-paddingless">
+						<span class="icon is-medium"><i class="fas fa-columns has-text-primary"></i></span>
+						Dashboard</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="navbar-end">
+			<h1 class="navbar-item is-paddingless">Username</h1>
+			<div class="navbar-item">
+				<a href="#">
+					<img src="https://bulma.io/images/placeholders/128x128.png" style="border-radius: 9001px;">
+				</a>
+			</div>
+		</div>
+	</div>
+</nav>
+
