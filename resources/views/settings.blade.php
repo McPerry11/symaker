a7325d14976a9c85a2a25ca09584073fd45a0888
jebjebs <virgiliolopez.jev@gmail.com>
2020-05-24 13:33:59 +0800

    V0.9.0 - Settings - Added profile settings, made text smaller on mobile viewport

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -6,9 +6,8 @@
 @endsection
 
 @section('body')
-<!-- add switch button -->
-
 <h1 class="subtitle is-3">Settings</h1>
+
 <div id="notifications" class="container settings-container">
 	<h2 class="subtitle is-size-4-tablet is-size-5-mobile is-marginless">Notifications</h2>
 	<hr class="settings-hr">
@@ -20,7 +19,7 @@
 			<span class="icon is-large is-hidden-mobile"><i class="fas fa-at fa-lg"></i></span>
 		</div>
 		<div class="column">
-			<a href="#" class="title is-6 is-spaced">Email Notification</a>
+			<a href="#" class="title is-size-7-mobile is-size-6-tablet is-spaced">Email Notification</a>
 			<p class="subtitle is-7 has-text-grey">Get notifications on	 your email.</p>
 		</div>
 	</div>
@@ -32,8 +31,37 @@
 			<span class="icon is-large is-hidden-mobile"><i class="fas fa-bell fa-lg"></i></span>
 		</div>
 		<div class="column">
-			<a href="#" class="title is-6 is-spaced">Push Notifications</a>
+			<a href="#" class="title is-size-7-mobile is-size-6-tablet is-spaced">Push Notifications</a>
 			<p class="subtitle is-7 has-text-grey">This will allow us to send push notifications to your device.</p>
+		</div>
+	</div>
+</div>
+
+<div id="profile" class="container settings-container">
+	<h2 class="subtitle is-size-4-tablet is-size-5-mobile is-marginless">Profile</h2>
+	<hr class="settings-hr">
+
+	<div class="columns is-mobile is-centered is-gapless">
+		<div class="column is-narrow">
+			<!-- icon is smaller on mobile viewport -->
+			<span class="icon is-medium is-hidden-tablet"><i class="fas fa-user-cog"></i></span>
+			<span class="icon is-large is-hidden-mobile"><i class="fas fa-user-cog fa-lg"></i></span>
+		</div>
+		<div class="column">
+			<a href="#" class="title is-size-7-mobile is-size-6-tablet is-spaced">Edit Username</a>
+			<p class="subtitle is-7 has-text-grey">Change your username</p>
+		</div>
+	</div>
+
+	<div class="columns is-mobile is-centered is-gapless">
+		<div class="column is-narrow">
+			<!-- icon is smaller on mobile viewport -->
+			<span class="icon is-medium is-hidden-tablet"><i class="fas fa-user-edit"></i></span>
+			<span class="icon is-large is-hidden-mobile"><i class="fas fa-user-edit fa-lg"></i></span>
+		</div>
+		<div class="column">
+			<a href="#" class="title is-size-7-mobile is-size-6-tablet is-spaced">Edit Photo</a>
+			<p class="subtitle is-7 has-text-grey">Change your profile picture.</p>
 		</div>
 	</div>
 </div>
@@ -49,7 +77,7 @@
 			<span class="icon is-large is-hidden-mobile"><i class="fas fa-key fa-lg"></i></span>
 		</div>
 		<div class="column">
-			<a href="#" class="title is-6 is-spaced">Change Password</a>
+			<a href="#" class="title is-size-7-mobile is-size-6-tablet is-spaced">Change Password</a>
 			<p class="subtitle is-7 has-text-grey">Change your current password.</p>
 		</div>
 	</div>
@@ -66,13 +94,11 @@
 			<span class="icon is-large is-hidden-mobile"><i class="fas fa-user-alt-slash fa-lg"></i></span>
 		</div>
 		<div class="column">
-			<a href="#" class="title is-6 is-spaced">Hide Profile</a>
+			<a href="#" class="title is-size-7-mobile is-size-6-tablet is-spaced">Hide Profile</a>
 			<p class="subtitle is-7 has-text-grey">When enabled, other users won't be able to see you.</p>
 		</div>
 	</div>
 </div>
-
-
 @endsection('body')
 
 
